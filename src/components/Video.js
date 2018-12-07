a3d6428400d25e979c649e71ed15d14f024ddd72
RAZOR <gbourson@student.42.fr>
2018-12-07 01:29:10 +0100

    Update Data

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 
 export default class Video extends Component{
     render(){
         const { src } = this.props
         const ImgStyle = {
             backgroundImage: 'url(' + src + ')',
-        } 
+        }
         return(
             <div className="video-container" style={ImgStyle}>
                 <div className="background-content" style={ImgStyle}></div>
                 <div className="play-container">
-
+                    <div className="play-button"></div>
+                    <p></p>
                 </div>
             </div>
         )   
     }
+}
+
+Video.propTypes = {
+    src: PropTypes.string,
 }
